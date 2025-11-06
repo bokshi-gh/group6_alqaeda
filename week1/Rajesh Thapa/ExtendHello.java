9cb56aa6cc1d4467179b21f9bff8303082c95bf7
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-06 13:00:35 +0545

    edit

--- week1/Rajesh Thapa/ExtendHello.java
+++ week1/Rajesh Thapa/ExtendHello.java
@@ -2,5 +2,6 @@ class ExtendHello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("This is my first program");
+		System.out.println("I am on module ST4003CEM");
 	}
 }
